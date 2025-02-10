afc992e70c08d0174759a95c15f36e80ce25f06b
Anshul Patel <anshulpatel2023@gmail.com>
2025-02-10 16:51:42 +0530

    first Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express=require('express');
+
+const app=express();
+
+const mongoose=require('mongoose');
+const listing=require("./models/listing.js");
+
+const murl="mongodb://127.0.0.1:27017/test";
+
+main().then(()=>{
+    console.log("Connected To MongoDB");
+}).catch(err=>{
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect(murl);
+};
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am Root");
+});
+
+app.get("/testlisting", async (req,res)=>{
+    let samplelisting=new listing({
+        title:"My New Villa",
+        description:"By The Beach",
+        price:1200,
+        location:"Calangute, Goa",
+        country:"India",
+    });
+
+    await samplelisting.save();
+    console.log("sample was saved");
+    res.send("successful testing");
+});
+
+app.listen(8080,()=>{
+    console.log("8080 is listening.....");
+});
+
---
+++ models/listing.js
@@ -0,0 +1,28 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const listSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        set:(v)=> v==="" ? "https://images.unsplash.com/photo-1738924349706-14d70715e236?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8":v,
+    },
+    price:{
+        type:Number,
+        requried:true,
+    },
+    location:{
+        type:String,
+        required:true,
+    },
+    country:String,
+});
+
+const listing=mongoose.model("listing",listSchema);
+
+module.exports=listing;
+
