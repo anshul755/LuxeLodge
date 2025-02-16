97e3887b70f82b81cd2a1e13f1040834b6fbb5ea
Anshul Patel <anshulpatel2023@gmail.com>
2025-02-16 15:40:50 +0530

    Further Changes Made

--- models/listing.js
+++ models/listing.js
@@ -4,23 +4,19 @@ const Schema=mongoose.Schema;
 const listSchema=new Schema({
     title:{
         type:String,
-        required:true,
     },
     description:String,
     image:{
-        filename:{type:String},
-        url:{
           type:String,
-          default:"https://images.unsplash.com/photo-1738924349706-14d70715e236?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
-        },
+          default:"https://images.unsplash.com/photo-1738924349706-14d70715e236?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8",
+          set: (v) =>
+            v === "" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60": v,
     },
     price:{
         type:Number,
-        requried:true,
     },
     location:{
         type:String,
-        required:true,
     },
     country:String,
 });
